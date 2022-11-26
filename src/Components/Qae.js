a24b84f3fc0973c2219586a006d2d96108edcb7e
Shah Waiz Iqbal <shahwaiz.iqbal125@gmail.com>
2022-11-26 16:17:32 +0500

    Create Qae.js

---
+++ src/Components/Qae.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Qae() {
+    return ( 
+        <div>
+            
+            <h1>I am QAae Trophy</h1>
+            
+        </div>
+     );
+}
+
+export default Qae;
