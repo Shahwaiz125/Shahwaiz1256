cb1a1888dbd23271c1a271b60f88fa40194a76e6
Shah Waiz Iqbal <shahwaiz.iqbal125@gmail.com>
2022-11-26 16:17:36 +0500

    Create Results.js

---
+++ src/Components/Results.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Results() {
+    return ( 
+        <div>
+            <h1>T20Worldcup</h1>
+        </div>
+     );
+}
+
+export default Results;
