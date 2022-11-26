fa9bd8fe255fe263d993dda767333d4efee435db
Shah Waiz Iqbal <shahwaiz.iqbal125@gmail.com>
2022-11-26 16:17:39 +0500

    Create T20.js

---
+++ src/Components/T20.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function T20() {
+    return ( 
+        <div>
+            
+            <h1>I am QAae Trophy</h1>
+            
+        </div>
+     );
+}
+
+export default T20;
