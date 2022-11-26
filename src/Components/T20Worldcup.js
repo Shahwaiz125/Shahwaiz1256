b76e45a1d223a959a033ffea03836bd264d26db0
Shah Waiz Iqbal <shahwaiz.iqbal125@gmail.com>
2022-11-26 16:17:42 +0500

    Create T20Worldcup.js

---
+++ src/Components/T20Worldcup.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function T20Worldcup() {
+    return ( 
+        <div>
+            
+            <h1>I am T20 Worldcup Trophy</h1>
+            
+        </div>
+     );
+}
+
+export default T20Worldcup;
